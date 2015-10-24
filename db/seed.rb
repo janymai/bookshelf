7dab1658fc3e6b3444100b5e1e29d125a326fbf8
My Mai <my.mai@asnet.com.vn>
2015-10-25 00:33:34 +0700

    create data

---
+++ db/seed.rb
@@ -0,0 +1,48 @@
+BookRepository.clear
+
+[
+  {
+    image_url: 'http://it-ebooks.org/img/books/wrox/professional_php6.jpg',
+    link: 'http://it-ebooks.org/book/wrox/professional_php6',
+    description: 'Publishing in tandem with the long-awaited release of PHP 6,
+                  this book reveals the inside scoop for pushing the limits of
+                  how to maximize the full feature set of PHP 6. You will learn
+                  how to use PHP 6 in the larger scheme of enterprise-class
+                  software development and practical examples and behind-the-scenes
+                  information will improve your skills for designing and building better
+                  large-scale, high-performance platforms using PHP 6.',
+    publisher: 'Wrox',
+    by: 'Ed Lecky-Thompson, Steven D. Nowicki',
+    isbn: '9780470395097',
+    year: DateTime.new(2009),
+    pages: 744,
+    languages: 'English',
+    file_size: '10.2MB',
+    file_type: 1,
+
+    created_at: DateTime.new(2015,4,4,4,5,6,'+7'),
+    updated_at: DateTime.new(2015,4,4,4,5,6,'+7'),
+  },
+  {
+    image_url: 'http://it-ebooks.org/img/books/wrox/professional_php6.jpg',
+    link: 'http://it-ebooks.org/book/wrox/professional_php6',
+    description: 'Publishing in tandem with the long-awaited release of PHP 6,
+                  this book reveals the inside scoop for pushing the limits of
+                  how to maximize the full feature set of PHP 6. You will learn
+                  how to use PHP 6 in the larger scheme of enterprise-class
+                  software development and practical examples and behind-the-scenes
+                  information will improve your skills for designing and building better
+                  large-scale, high-performance platforms using PHP 6.',
+    publisher: 'Wrox',
+    by: 'Ed Lecky-Thompson, Steven D. Nowicki',
+    isbn: '9780470395097',
+    year: DateTime.new(2009),
+    pages: 744,
+    languages: 'English',
+    file_size: '10.2MB',
+    file_type: 1,
+
+    created_at: DateTime.new(2015,4,4,4,5,6,'+7'),
+    updated_at: DateTime.new(2015,4,4,4,5,6,'+7'),
+  }
+]
