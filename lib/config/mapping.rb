7a86020acee9ccd9f106a6aeb3d5d5adf20df6d7
My Mai <my.mai@asnet.com.vn>
2015-11-16 08:29:32 +0700

    config mapping data

--- lib/config/mapping.rb
+++ lib/config/mapping.rb
@@ -17,3 +17,13 @@ collection :books do
   attribute :created_at, DateTime
   attribute :updated_at, DateTime
 end
+
+collection :users do
+  entity User
+  repository UserRepository
+
+  attribute :id, Integer
+  attribute :name, String
+  attribute :email, String
+  attribute :password_digest, String
+end
