4d75bbf52e34241cc5dc94797bf1e402ea946265
My Mai <my.mai@asnet.com.vn>
2015-11-22 22:51:27 +0700

    method helper in view

--- apps/web/views/books/show.rb
+++ apps/web/views/books/show.rb
@@ -1,5 +1,11 @@
 module Web::Views::Books
   class Show
     include Web::View
+
+    # view se xu ly nhung method render san cho minh,
+    # lay method nay binding o template
+    def year
+      book.year.strftime("%Y")
+    end
   end
 end
